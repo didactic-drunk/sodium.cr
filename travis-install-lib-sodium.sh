8a18415185f20bc7657a0002fad4c45fd0a293d0
Andrew Hamon <andrew@hamon.cc>
2018-02-16 16:19:56 -0500

    Use LATEST rather than specific libsodium release in tests

--- travis-install-lib-sodium.sh
+++ travis-install-lib-sodium.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # The purpose of this file is to install libsodium in
-# the Travis CI environment. Outside this environment,
-# you would probably not want to install it like this.
-#
-# from: google/hat-backup
+# the Travis CI environment. We recommend using a
+# package manager.
 
 set -e
 
-wget https://download.libsodium.org/libsodium/releases/libsodium-stable-2018-02-14.tar.gz
-tar xvfz libsodium-stable-2018-02-14.tar.gz
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar xvfz LATEST.tar.gz
 cd libsodium-stable
 sudo ./configure
 sudo make
