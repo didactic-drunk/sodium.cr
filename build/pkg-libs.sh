b227b4b1898c6d4d2a2df3a374e389d570c01e23
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-03-19 16:40:41 +1100

    feat(libsodium_install): make libsodium_install.sh an executable

--- build/pkg-libs.sh
+++ build/pkg-libs.sh
@@ -5,7 +5,7 @@ set -e
 # Shard directory passed as first argument when called from lib_sodium.cr
 [ ! -z "$1" ] && cd "$1"
 
-./build/libsodium_install.sh > libsodium_install.out 2>&1 || (cat libsodium_install.out >&2 ; exit 2)
+./build/libsodium_install > libsodium_install.out 2>&1 || (cat libsodium_install.out >&2 ; exit 2)
 
 . ./build/env.sh
 
